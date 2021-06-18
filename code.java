776d79c5b26718f965b81296072124cd2bcee868
Fikri San Koktas <kokfik.21@robcol.k12.tr>
2021-06-18 21:57:14 +0300

    Update and rename README.md to code.java

--- code.java
+++ code.java
@@ -1,15 +1,11 @@
 # RadiusCalculations
 This program asks for the radius value and then calculates the area and circumference of th circle. 
 
-    import java.util.Scanner;
-
-    public class KFS_RadiusCalculations_Main
-
+import java.util.Scanner;
+public class KFS_RadiusCalculations_Main
     {
-   
     public static void main(String [] args)
-    
-    {
+        {
         Scanner in = new Scanner(System.in);
         System.out.print("Please enter the radius value: "); //user will plug in a number here
         double radius = in.nextDouble();
@@ -19,6 +15,5 @@ This program asks for the radius value and then calculates the area and circumfe
         
         double area = Math.PI*radius*radius; //user's radius will be converted to area
         System.out.println(area);
-    }
-    
+        }
     }
